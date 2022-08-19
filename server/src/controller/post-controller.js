b2593dd7173b62a7b55e198ca7f77d4aa295a804
Taesik yoon <taesikyoon97@naver.com>
2022-08-19 23:35:20 +0900

    Create function Check all posts and check detailed posts

--- server/src/controller/post-controller.js
+++ server/src/controller/post-controller.js
@@ -1,10 +1,13 @@
 import PostService from "../services/post-service.js";
 class PostController {
   postService = new PostService();
+
   createpost = async (req, res, next) => {
     const { content, image } = req.body;
 
-    if (!content || !image) throw Error("Error no item");
+    if (!content || !image)
+      // 인스타그램 content는 null 허용 수정 필요
+      res.status(409).json({ sucess: false, message: "이미지 넣어주세요" });
 
     try {
       await this.postService.createpost(content, image);
@@ -12,9 +15,35 @@ class PostController {
         .status(200)
         .json({ sucess: true, message: "게시글이 작성되었습니다." });
     } catch (err) {
+      console.error(err);
+      next(err);
+    }
+  };
+
+  findAllPosts = async (req, res, next) => {
+    try {
+      const data = await this.postService.findAllPosts();
+
       res
-        .status(400)
-        .json({ sucess: false, message: "게시글 작성에 실패했습니다." });
+        .status(200)
+        .json({ sucess: true, message: "전체 게시글 조회 성공.", data });
+    } catch (err) {
+      console.error(err);
+      next(err);
+    }
+  };
+
+  findOnePost = async (req, res, next) => {
+    const { postId } = req.params;
+
+    try {
+      const data = await this.postService.findOnePost(postId);
+      res
+        .status(200)
+        .json({ sucess: true, message: "상세 게시글 조회 성공.", data });
+    } catch (err) {
+      console.error(err);
+      next(err);
     }
   };
 }
--- server/src/services/post-service.js
+++ server/src/services/post-service.js
@@ -5,7 +5,44 @@ class PostService {
     const data = await Post.create({ content, image });
 
     if (data) return true;
-    else throw Error(false);
+    else {
+      const error = new Error("게시글 작성 실패");
+      error.status = 400;
+      throw error;
+    }
+  };
+
+  findAllPosts = async () => {
+    const lists = await Post.findAll({});
+
+    return lists.map((list) => {
+      return {
+        id: list.id,
+        content: list.content,
+        image: list.image,
+        userId: "userId가져올예정",
+        commentCnt: "comment가져올예정",
+        postlikes: "postlikes가져올예정",
+      };
+    });
+  };
+
+  findOnePost = async (id) => {
+    const list = await Post.findOne({ where: { id } });
+
+    if (!list) {
+      const error = new Error("게시글이 존재하지 않습니다.");
+      error.status = 418;
+      return error;
+    }
+    return {
+      id: list.id,
+      content: list.content,
+      image: list.image,
+      userId: "userId가져올예정",
+      commentCnt: "comment가져올예정",
+      postlikes: "postlikes가져올예정",
+    };
   };
 }
 
