5bf168fdc8dbab93d8bb8b8a3723c5e2aaa6de6f
Rumaro122 <yeshin0122@gmail.com>
2022-08-22 03:45:05 +0900

    user-detail 모델 삭제 및 관련 관계 정의 수정

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -4,23 +4,19 @@ import sequelize from "./sequelize.js";
 import User from "./user.js";
 import Post from "./post.js";
 import Comment from "./comment.js";
-import UserDetail from "./user-detail.js";
 
 const db = {};
 
 db.sequelize = sequelize;
 db.User = User;
-db.UserDetail = UserDetail;
 db.Post = Post;
 db.Comment = Comment;
 
 User.init(sequelize);
-UserDetail.init(sequelize);
 Post.init(sequelize);
 Comment.init(sequelize);
 
 User.associate(db);
-UserDetail.associate(db);
 Post.associate(db);
 Comment.associate(db);
 db.sequelize.models.Like;
