77ba3dda5fdb57ed3932db95afd35a99fd0b3e51
Rumaro122 <yeshin0122@gmail.com>
2022-08-22 03:49:36 +0900

    프로필 수정 및 user 관련 기능들 user-detail 모델 없이 작동하게 변경 && 마마이 피드 기능 추가

--- server/src/repositories/user.repository.js
+++ server/src/repositories/user.repository.js
@@ -1,62 +1,102 @@
 import User from '../models/user.js';
+import Post from '../models/post.js';
 
 class UserRepository {
    getUser = async (nickname) => {
-      try{
+      try {
          const user = await User.findOne({
             where: {
                nickname,
             },
          });
          return user;
-      }catch(err){
-         const error= new Error("FAILD_SQL");
-         error.code=405 ;
+      } catch (err) {
+         const error = new Error("FAILD_SQL");
+         error.code = 405;
          throw error;
       }
    };
-   createUser = async (name, password,nickname) => {
-      try{
+   createUser = async (name, password, nickname) => {
+      try {
          const create = await User.create({
             name,
             password,
             nickname,
          });
          return create;
-      }catch(err){
-         const error= new Error("FAILD_SQL");
-         error.code=405 ;
+      } catch (err) {
+         const error = new Error("FAILD_SQL");
+         error.code = 405;
          throw error;
       }
    };
-   getNickname=async(nickname)=>{
-      try{
+
+   getNickname = async (nickname) => {
+      try {
          const nick = await User.findOne({
             where: {
                nickname,
             },
          });
          return nick;
-      }catch(err){
-         const error= new Error("FAILD_SQL");
-         error.code=405 ;
+      } catch (err) {
+         const error = new Error("FAILD_SQL");
+         error.code = 405;
          throw error;
-      }
-   }
-   getNicknameById=async(userId)=>{
-      try{
-         const nickname=await User.findOne({
-            where:{
-               userId
+      };
+   };
+
+   getNicknameById = async (id) => {
+      try {
+         const nickname = await User.findOne({
+            where: {
+               id
             }
          })
          return nickname
-      }catch(err){
-         const error= new Error("FAILD_SQL");
-         error.code=405 ;
+      } catch (err) {
+         const error = new Error("FAILD_SQL");
+         error.code = 405;
          throw error;
+      };
+   };
+      
+   updateProfile = async ( name, nickname, profile, introduce, email, phone, gender, id) => {
+      console.log(id)
+      try{
+         return await User.update({
+            name: name, 
+            nickname: nickname, 
+            profile: profile, 
+            introduce: introduce, 
+            email: email, 
+            phone: phone, 
+            gender: gender
+         },
+            { where: { id : id } },
+         )         
+      } catch (err) {
+         const error = new Error("FAILD_SQL_UP");
+            error.code = 405;
+            throw error
+      };
+   };
+
+   getMyFeed = async(id) => {
+      try{         
+         const response = await Post.findAll({
+            order: [['created_at', 'DESC']],
+            where: { fk_user_id : id},
+            attributes: {exclude: ['fk_user_id']}
+         });   
+         console.log(response)
+         return response;
+      } catch (err) {
+         const error = new Error("FAILD_SQL");
+         error.code = 405;
+         throw error
       }
-   }
+   };
 }
 
 export default UserRepository;
