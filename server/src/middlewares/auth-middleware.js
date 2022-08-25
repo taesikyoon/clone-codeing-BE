8df5934b169a6ffcc1c3602e676c91690810b316
Rumaro122 <yeshin0122@gmail.com>
2022-08-25 09:45:11 +0900

    미들웨어 수정 굳이 필요는 없지만 혹시나 하는 마음에 주석처리해둠

--- server/src/middlewares/auth-middleware.js
+++ server/src/middlewares/auth-middleware.js
@@ -10,45 +10,46 @@ const authMiddlewares = async (req, res, next) => {
   //    const { authorization } = req.headers;
 
   const { authorization } = req.headers;
-
-
-  console.log(req.query.token)
-  if (req.query.token) {
-    try {
-      const verifyToken = jwt.verify(req.query.token, process.env.SECRET_KEY);
-      res.locals.nickname = verifyToken.nickname;
-      res.locals.id = verifyToken.id;
-      next();
-      return
-      
-    } catch (err) {
-      console.log(err);
-      err.message = "유효하지 않은 토큰입니다.";
-      next(err);
-      return
-    }   
-
+  
   if (!authMiddlewares) {
     return res
       .status(400)
       .json({ success: false, message: "로그인이 필요합니다." });
-  }
-  const token = authorization.split(" ");
-  if (token[0] !== "Bearer" || !token)
-    return res
-      .status(401)
-      .json({ success: false, message: "로그인이 필요합니다." });
+  };
 
-  try {
-    const verifyToken = jwt.verify(token[1], process.env.SECRET_KEY);
-    res.locals.nickname = verifyToken.nickname;
-    res.locals.id = verifyToken.id;
-    next();
-  } catch (err) {
-    console.log(err);
-    err.message = "유효하지 않은 토큰입니다.";
-    next(err);
-  }
+  // if (req.query.token) {
+  //   try {
+  //     const verifyToken = jwt.verify(req.query.token, process.env.SECRET_KEY);
+  //     res.locals.nickname = verifyToken.nickname;
+  //     res.locals.id = verifyToken.id;
+  //     console.log(res.locals.id)
+  //     next();
+  //     return
+      
+  //   } catch (err) {
+  //     console.log(err);
+  //     err.message = "유효하지 않은 토큰입니다.";
+  //     next(err);
+  //     return
+  //   } 
+  // } else {}
+    const token = authorization.split(" "); 
+    if (token[0] !== "Bearer" || !token)
+      return res
+        .status(401)
+        .json({ success: false, message: "로그인이 필요합니다." });
+  
+    try {
+      const verifyToken = jwt.verify(token[1], process.env.SECRET_KEY);
+      res.locals.nickname = verifyToken.nickname;
+      res.locals.id = verifyToken.id;
+      next();
+    } catch (err) {
+      console.log(err);
+      err.message = "유효하지 않은 토큰입니다.";
+      next(err);
+    } 
+
 };
 
 export default authMiddlewares;
